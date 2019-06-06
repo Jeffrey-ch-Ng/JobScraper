1b62268adb737abc00d24bf30f927498a7e5c43e
jeffrey-ch-ng <ng.jeffrey@rogers.com>
2019-06-06 19:09:07 -0400

    add indeed elements

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,8 @@
 import urllib
 from bs4 import BeautifulSoup
-
 import requests
+import pandas as pd
+
 
 def format_html(word):
     for x in range(len(word)):
@@ -10,25 +11,54 @@ def format_html(word):
         else:
             return word
 
+def format_links(site_link, link):
+    return (site_link + link)
 
-indeed = requests.get("https://www.indeed.ca/jobs?q=software+co-op&l=Toronto")
-print(indeed)
+def get_indeed():
+    indeed = requests.get("https://www.indeed.ca/jobs?q=software+co-op&l=Toronto")
+    print(indeed)
 
-soup = BeautifulSoup(indeed.content, 'html.parser')
-#print(soup.prettify())
-jobs_column = soup.find(id='resultsCol')
+    soup = BeautifulSoup(indeed.content, 'html.parser')
 
-job_names_container = jobs_column.select("#resultsCol .jobsearch-SerpJobCard .title a")
+    jobs_column = soup.find(id='resultsCol')
 
-job_names = [name.get_text() for name in job_names_container]
+    job_names_container = jobs_column.select("#resultsCol .jobsearch-SerpJobCard .title a.jobtitle")
+    job_names = [name.get_text() for name in job_names_container]
 
-for name in job_names:
-    name = format_html(name)
-    print(name)
+    job_companies_container = jobs_column.select("#resultsCol .jobsearch-SerpJobCard .sjcl span.company")
+    job_companies = [company.get_text() for company in job_companies_container]
+
+    job_location_container = jobs_column.select("#resultsCol .jobsearch-SerpJobCard .sjcl .location")
+    job_location = [location.get_text() for location in job_location_container]
+
+    job_description_container = jobs_column.select("#resultsCol .jobsearch-SerpJobCard .summary")
+    job_description = [description.get_text() for description in job_description_container]
+
+    job_links_container = jobs_column.select("#resultsCol .jobsearch-SerpJobCard .title a[href]")
+    job_links = [links.get("href") for links in job_links_container]
+
+    for x in range(len(job_names)):
+        job_names[x] = format_html(job_names[x])
+        job_companies[x] = format_html(job_companies[x])
+        job_location[x] = format_html(job_location[x])
+        job_description[x] = format_html(job_description[x])
+        job_links[x] = format_links("https://www.indeed.ca", job_links[x])
 
 
+    job = pd.DataFrame({
+        "job_names": job_names,
+        "job_companies": job_companies,
+        "job_locations": job_location,
+        "job_description": job_description,
+        "job_links": job_links
+    })
+    pd.set_option('display.max_colwidth', -1)
+
+    print(job)
 
 
-#print(soup.find_all(class_="title2font"))
-#tags = [tag.get_text() for tag in title_tags]
-#print(tags)
+def main():
+    get_indeed()
+
+if __name__== "__main__":
+    main()
